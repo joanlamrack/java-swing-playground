27c98a9abd19e40394bfdc790a270888340d66ee
Joan Lamrack <joanlamrack@gmail.com>
2018-11-02 09:05:07 +0700

    naming convention fixe

--- constants.java
+++ constants.java
@@ -1,7 +1,8 @@
 import java.util.Date;
 
 public class constants{
-	public static final String name = "Erithiana Sisijoan Koesnadi";
-	public static final Date birthdate = new Date(1995, 8, 9);
-	public static final String occupation = "Full stack developer";
+	public static final String NAME = "Erithiana Sisijoan Koesnadi";
+	public static final Date BIRTHDATE = new Date(1995, 8, 9);
+	public static final String OCCUPATION = "Full stack developer";
+	public static final String CURRENT_WORKPLACE = "Xendit"; 
 }
