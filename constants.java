cbcd104d61823ba1adbdbc7bfb1250f8c0efcadc
Joan Lamrack <joanlamrack@gmail.com>
2018-11-02 09:03:22 +0700

    attempt at creating contants and MVC swing

---
+++ constants.java
@@ -0,0 +1,7 @@
+import java.util.Date;
+
+public class constants{
+	public static final String name = "Erithiana Sisijoan Koesnadi";
+	public static final Date birthdate = new Date(1995, 8, 9);
+	public static final String occupation = "Full stack developer";
+}
