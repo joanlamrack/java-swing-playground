c9e0ee74da4e012b12d1b86be8d992b628777925
Joan Lamrack <joanlamrack@gmail.com>
2018-11-03 05:57:47 +0700

    Added model

--- model.java
+++ model.java
@@ -1,3 +1,9 @@
-public class model{
-	
+import javax.swing.table.DefaultTableModel;
+
+import com.sun.org.apache.bcel.internal.classfile.Constant;
+
+public class model extends DefaultTableModel{
+	public model(){
+		super(constants.DATA, constants.TABLE_HEADER);
+	}
 }
